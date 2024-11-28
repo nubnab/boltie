bd918616ecd35ee9e84cb0e3fa9503e24375ecca
nubnab <emkata654@gmail.com>
2024-11-28 22:21:33 +0200

    Angular frontend added

--- backend/src/main/java/com/boltie/backend/dto/UserDto.java
+++ backend/src/main/java/com/boltie/backend/dto/UserDto.java
@@ -13,5 +13,5 @@ public class UserDto {
 
     private Long id;
     private String username;
-
+    private String token;
 }
---
+++ backend/src/main/java/com/boltie/backend/enums/Role.java
@@ -0,0 +1,14 @@
+package com.boltie.backend.enums;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ;
+
+    @Override
+    public String getAuthority() {
+        return this.name();
+    }
+}
