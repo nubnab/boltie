467fc3fe1b43afc76a59238c59315c5ca8a843bf
nubnab <emkata654@gmail.com>
2024-12-06 14:05:29 +0200

    Added test thumbnails and grid in "Live now"

--- frontend/src/app/pages/live/live.component.ts
+++ frontend/src/app/pages/live/live.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {MatCard, MatCardHeader, MatCardImage, MatCardTitle} from '@angular/material/card';
+
+export type StreamCardContent = {
+  title: string;
+  imageUrl: string;
+};
 
 @Component({
   selector: 'app-live',
-  imports: [],
+  imports: [
+    MatCardTitle,
+    MatCardHeader,
+    MatCard,
+    MatCardImage
+  ],
   templateUrl: './live.component.html',
   styleUrl: './live.component.scss'
 })
 export class LiveComponent {
+
+  cards = signal<StreamCardContent[]>([]);
+
+  images = [
+    '/thumb-test/thumb-2160p.jpeg',
+    '/thumb-test/thumb-1440p.jpeg',
+    '/thumb-test/thumb-1080p.jpeg',
+    '/thumb-test/thumb-720p.jpeg',
+    '/thumb-test/thumb-480p.jpeg',
+    '/thumb-test/thumb-360p.jpeg',
+    '/thumb-test/thumb-240p.jpeg',
+    '/thumb-test/thumb-144p.jpeg',
+  ]
+
+  constructor() {
+    const cards: StreamCardContent[] = [];
+
+    for(let i = 0; i < this.images.length; i++) {
+      cards.push({
+        title: `Card ${i + 1}`,
+        imageUrl: this.images[i],
+      })
+    }
+
+    this.cards.set(cards);
+
+  }
 
 }
