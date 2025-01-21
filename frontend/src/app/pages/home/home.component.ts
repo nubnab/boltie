f12b845023f30ade678811c58f4d6b4b2e487ea9
nubnab <emkata654@gmail.com>
2025-01-21 12:54:13 +0200

    Refactored JS card DOM into angular template.

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -3,6 +3,12 @@ import {AuthService} from '../../services/auth.service';
 import {RequestsService} from '../../services/requests.service';
 import {Subject, takeUntil} from 'rxjs';
 import {Router} from '@angular/router';
+import {
+  MatCard,
+  MatCardSubtitle,
+  MatCardTitle,
+  MatCardTitleGroup
+} from '@angular/material/card';
 
 export type StreamDetails = {
   username: string;
@@ -12,13 +18,19 @@ export type StreamDetails = {
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatCardTitleGroup,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 
 
 export class HomeComponent implements OnInit, OnDestroy {
+  streams: StreamDetails[] = [];
   private destroy$= new Subject<void>();
 
   private authService = inject(AuthService);
@@ -40,11 +52,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     return this.requestsService.getLiveStreamInfo()
       .pipe(takeUntil(this.destroy$))
       .subscribe((streams) => {
-
-      if (streams && streams.length > 0) {
-        console.log(streams);
-        this.renderGrid(streams);
-      }
+      this.streams = streams;
     });
   }
 
@@ -52,89 +60,8 @@ export class HomeComponent implements OnInit, OnDestroy {
     return `http://192.168.1.2:20080/boltie/${username}_preview/thumb.jpg`;
   }
 
-  renderGrid(streams: StreamDetails[]) {
-    const documentRef = document.getElementById('responsive-grid');
-
-    if(documentRef) {
-        documentRef.innerHTML = ``;
-        streams.forEach((stream => {
-          for (let i = 0; i < 6; i++) {
-            this.renderCard(documentRef, stream);
-          }
-        }));
-      }
+  navigateToStream(username: string) {
+    this.router.navigate([`/${username}`]);
   }
-
-  renderCard(documentRef: HTMLElement, stream: StreamDetails) {
-    const matCard = document.createElement('mat-card');
-    const matCardHeader = document.createElement('mat-card-header');
-    const matCardTitle = document.createElement('mat-card-title');
-    const matCardSubtitle = document.createElement('mat-card-subtitle');
-    const thumbnailImgContainer = document.createElement('div');
-    const profileImgContainer = document.createElement('div');
-    const profileImg = document.createElement('img');
-    const thumbnailImg = document.createElement('img');
-    const cardContentContainer = document.createElement('div');
-
-
-    thumbnailImg.src = this.generateThumbnail(stream.username);
-    thumbnailImg.style.width = '100%';
-    thumbnailImg.style.height = 'auto';
-    thumbnailImg.style.display = 'block';
-    profileImg.style.objectFit = 'cover';
-    profileImg.style.width = '40px';
-    profileImgContainer.style.display = 'flex';
-    profileImgContainer.style.alignItems = 'center';
-    profileImgContainer.style.borderRadius = '100%';
-    profileImgContainer.style.overflow = 'hidden';
-    profileImgContainer.style.margin = '10px';
-    matCard.style.borderRadius = '8px';
-    matCard.style.overflow = 'hidden';
-    thumbnailImg.style.borderRadius = '8px';
-    thumbnailImgContainer.style.overflow = 'hidden';
-    profileImg.src = '/default-profile-picture.png';
-    matCardTitle.innerHTML = stream.title;
-    matCardTitle.style.fontSize = '1.2rem';
-    matCardTitle.style.fontWeight = '500';
-    matCardSubtitle.innerHTML = stream.username;
-    matCardSubtitle.style.fontSize = '0.9rem';
-    matCardSubtitle.style.color = '#606060';
-    matCardHeader.style.display = 'flex';
-    matCardHeader.style.flexDirection = 'column';
-    matCardHeader.style.justifyContent = 'center';
-    matCard.addEventListener('click', () => {
-      this.router.navigate([`/${stream.username}`]);
-    })
-    cardContentContainer.style.display = 'flex';
-
-    matCard.style.cursor = 'pointer';
-
-    matCard.addEventListener('mouseenter', () => {
-      matCard.style.backgroundColor = '#e1e1e2';
-    });
-    matCard.addEventListener('mouseleave', () => {
-      matCard.style.backgroundColor = '#faf9fd';
-    });
-
-
-
-    profileImgContainer.appendChild(profileImg);
-    thumbnailImgContainer.appendChild(thumbnailImg);
-
-
-    matCardHeader.appendChild(matCardTitle);
-    matCardHeader.appendChild(matCardSubtitle);
-
-    matCard.appendChild(thumbnailImgContainer);
-    matCard.appendChild(profileImgContainer);
-
-    cardContentContainer.appendChild(profileImgContainer);
-    cardContentContainer.appendChild(matCardHeader);
-
-    matCard.appendChild(cardContentContainer);
-
-    documentRef.appendChild(matCard);
-  }
-
 
 }
