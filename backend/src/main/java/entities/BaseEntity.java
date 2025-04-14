ef3f6bd831e3569f873fac9636d841bd5d69c8a4
nubnab <emkata654@gmail.com>
2025-04-14 23:03:45 +0300

    Fix annotations

--- backend/src/main/java/entities/BaseEntity.java
+++ backend/src/main/java/entities/BaseEntity.java
@@ -7,11 +7,13 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.util.UUID;
 
 @MappedSuperclass
 @Data
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 public class BaseEntity {
